200417a83fe0f4e519698872182579d0e0a2e818
lamnguyenx <lamfm95@gmail.com>
2024-09-15 10:49:16 +0700

    /// cpp

--- projects/hanoi_learn_cpp/src/main.cpp
+++ projects/hanoi_learn_cpp/src/main.cpp
@@ -14,34 +14,10 @@ using namespace std;
 using json = nlohmann::json;
 
 // local
+#include <hanoi_learn_cpp/tour/functions_and_closures.hpp>
 #include <hanoi_learn_cpp/tour/objects_and_classes.hpp>
 
-
 // OPERATIONS
-bool f1(string s1, string s2) { return s1.length() > s2.length(); }
-
-tuple<int, float> f2(float num) {
-    int num_i = static_cast<int>(num);
-    float num_f = num - num_i;
-    return make_tuple(num_i, num_f);
-}
-
-// clang-format off
-// Functions are a first-class type
-int f6_1(string name) { return name.length(); }
-
-// Passing function as an argument
-int f6_3(
-
-    function<int(string)> fx,
-    string name
-
-) {
-    return fx(name);
-}
-
-
-
 int main() {
     cout << boolalpha;
     string intro = R"(
@@ -201,42 +177,7 @@ U
         << endl;
     // clang-format on
 
-    // function
-    bool f1_r1 = f1("hello", "nobita");
-    cout << boolalpha << f1_r1 << endl;  // Output: false
-
-    auto f2_r2 = f2(6.9);
-
-    int f2_r2_i;
-    float f2_r2_f;
-    tie(f2_r2_i, f2_r2_f) = f2(6.9);
-
-    cout << f2_r2_i << ", " << f2_r2_f << endl;
-
-    // closure / lambda
-    auto f4 = [](int x, int y) { return x + y; };
-    int f4_r = f4(3, 5);
-    cout << f4_r << endl;
-
-    auto f5 = [](int x, int y) {
-        int z = 10;
-        return x + y + z;
-    };
-    int f5_r = f5(3, 5);
-    cout << f5_r << endl;
-
-    // Assigning function to a variable
-    function<int(const string&)> f6_2 = f6_1;
-    int f6_2_r = f6_2("rick");
-
-    int f6_3_r = f6_3(
-
-        f6_1, "morty");
-
-    // Print results
-    cout << f6_2_r << endl;  // Output: 4
-    cout << f6_3_r << endl;  // Output: 5
-
+    functions_and_closures();
     objects_and_classes();
 
     // -- END --
